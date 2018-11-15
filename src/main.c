5b78261fea9b55256e851e151dd2d4020ae22690
narumugamge1 <44801227+narumugamge1@users.noreply.github.com>
2018-11-15 20:17:30 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,40 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
-#include <stdio.h>
+#include "stdlib.h"
 
 int main(void)
 {
-	printf("Hello World");
-	
-    return 0;
+unsigned long int n=0,S1=0,r,S2=0;S3=0,S4=0;
+unisgned short int m=0,i=0,a=0,b=0,p=0;
+	while(n<146)
+	{
+		S1=S1+n;
+		n=n+3;
+	}
+	while(n2<9876)
+	{
+		S2=S2+M;
+		m=m+3;
+	}
+	while(i<13)
+	{
+		r=(rand()%10)+1;
+		S3=S3+r;
+		i=i+1;
+	}
+	while(p<21)
+	{
+		if(a%3==0||b%7==0)
+		{
+			S4=S4+a;
+			p=p+1;
+			a=a+1;
+			b=b+1;
+		}
+		else
+		{
+			a=a+1;
+			b=b+1;
+		}
+	}
+	return 0;
 }
