7d01ae80edb48dce0ce678d15afdd26f113348ba
narumugamge1 <44801227+narumugamge1@users.noreply.github.com>
2018-11-15 19:13:16 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,11 +2,41 @@
 //
 
 #include "my_header.h"
-#include <stdio.h>
+#include "TP_lib.h"
 
 int main(void)
+
 {
-	printf("Hello World");
+int nb,point=0;
+
+nb = LancerDe ();
+
+while (!nb==1)
+{
+	if (nb==2*nb)
+	{
+		point=point+nb;
+	}
 	
+	else if(nb==3)
+	{
+		point=point*2;
+	}
+	
+	else if(nb==5)
+	{
+		if(point>=2)
+		{
+			point=point-2;
+		}
+		else
+		{
+		point=0;
+			
+		}
+	
+	}
+		
+}
     return 0;
 }
