595d553b935094bba83c9f32a6e4cd988d433a32
narumugamge1 <44801227+narumugamge1@users.noreply.github.com>
2018-11-15 19:21:35 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,34 +9,34 @@ int main(void)
 {
 int nb,point=0;
 
-nb = LancerDe ();
+nb = LancerDe ();//nb c'est le lancer de dés
 
-while (!nb==1)
+while (!nb==1)//il n'obtient pas de 1
 {
-	if (nb==2*nb)
+	if (nb==2*nb)// si nb est pariun nombre pair
 	{
-		point=point+nb;
+		point=point+nb;//il augmentye ses points du chiffre obtenue
 	}
 	
-	else if(nb==3)
+	else if(nb==3)//si le nombre vaut 3
 	{
-		point=point*2;
+		point=point*2;//les points sont multipliés par 2
 	}
 	
-	else if(nb==5)
+	else if(nb==5)// et si le nombre vaut 5
 	{
-		if(point>=2)
+		if(point>=2) //si les point sont au dessus de 0
 		{
-			point=point-2;
+			point=point-2;// il perd deux points
 		}
 		else
 		{
-		point=0;
+		point=0//fin du tour
 			
 		}
 	
 	}
-		
+}		
+	return 0;//retour d'une valeur
 }
-    return 0;
-}
+
