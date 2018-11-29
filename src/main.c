87acdda49fcb8c9e056fa0211cb949f7d97fe53a
nisshanth arumugam <nisshanth.arumugam@ge1.iut-cachan.u-psud.fr>
2018-11-29 14:14:45 +0100

    fini partie 4

--- src/main.c
+++ src/main.c
@@ -2,8 +2,38 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
 
 int main(void)
 {
-    return 0;
+
+int bn=0,i=0,j=0;	
+unsigned char joueur[6]={2,45,32,14,5,22};
+unsigned char gagnante[6]={0,0,0,0,0,0};
+    
+	initialiserTirage(); 
+	for(i=0;i<6;i++)					   
+	{
+		gagnante[i]=tirerNumero();
+	}
+	
+
+
+	 
+	for(i=0;i<6;i++)					   
+	{
+		for(j=0;j<6;j++)
+		{
+
+			if(joueur[i]==gagnante[j])
+			{
+				bn=bn+1;
+			}
+		}
+	}
+	
+
+
+	return 0;
+
 }
