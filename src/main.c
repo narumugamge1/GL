32e14783f7073dce7172e63ba6693400198f7ceb
nisshanth arumugam <nisshanth.arumugam@ge1.iut-cachan.u-psud.fr>
2018-12-04 12:03:04 +0100

    exo 79 fini

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <userint.h>
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
-    return 0;
+
+	char nom[100] ;
+	char prenom[100] ;
+	char annee [100] ;
+	char phrase [100] ;
+
+	
+	scanf("%s",nom);
+	scanf("%s",prenom);
+	scanf("%s",annee);
+	
+	
+	
+	sprintf(phrase,"je m'appelle %s,%s,et je suis n�e en%s",nom,prenom,annee);
+	
+	MessagePopup("Le contenu de ta chaine est",phrase);
+	
+	return 0;
+
+	
 }
